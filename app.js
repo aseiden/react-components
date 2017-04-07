47cc4c8b35f92121feb7d178b461e66cda6d3931
Alex Seiden <alex.seiden@gmail.com>
2017-04-07 12:17:50 -0700

    complete excercise

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+      {props.items.map(item =>
+        <GroceryListItem item={item} />
+      )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render () {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+
+/*var GroceryListItems = (props) => {
+  var onListItemClick = (event) => {
+    console.log('i been clicked');
+  }
+  
+  return <ul>
+    <li onClick={onListItemClick}>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+    <li>{props.items[2]}</li>
+  </ul>
+};*/
+
+ReactDOM.render(<GroceryList items={['ham', 'eggs', 'cheese', 'yolo']}/>, document.getElementById('app'));
